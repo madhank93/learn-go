ae3595dacdeeb6f5eed5c8f60e077b00a04a22da
madhank93 <madhankumaravelu93@gmail.com>
2023-03-10 23:05:57 +0530

    feat: added impl for other routers

--- movies-crud/main.go
+++ movies-crud/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -27,13 +29,57 @@ func main() {
 	movies = append(movies, Movie{ID: "001", Title: "Fight club", Director: &Director{FirstName: "David", LastName: "Fincher"}})
 	movies = append(movies, Movie{ID: "002", Title: "Gone Girl", Director: &Director{FirstName: "David", LastName: "Fincher"}})
 
-	r.Get("/movies", getMovies)
+	r.Get("/movies", listMovies)
+	r.Get("/movie/{id}", getMovie)
+	r.Post("/movie", createMovie)
+	r.Put("/movie/{id}", updateMovie)
+	r.Delete("/movie/{id}", deleteMovie)
 
 	fmt.Printf("Starting server on 8080 \n")
 	http.ListenAndServe(":8080", r)
 }
 
-func getMovies(w http.ResponseWriter, r *http.Request) {
+func listMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	for _, movie := range movies {
+		if movie.ID == chi.URLParam(r, "id") {
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Print(r.Body)
+	for index, movie := range movies {
+		if movie.ID == chi.URLParam(r, "id") {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			fmt.Print(movie)
+			movie.ID = chi.URLParam(r, "id")
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	for index, movie := range movies {
+		if movie.ID == chi.URLParam(r, "id") {
+			movies = append(movies[:index], movies[index+1:]...)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 }
