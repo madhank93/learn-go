9b8acc3a86a0164ac4d1e8a20c5951994a93a7c8
madhank93 <madhankumaravelu93@gmail.com>
2023-03-21 21:31:15 +0530

    feat: completed book store management crud operation

--- book-store-management/pkg/controller/book-controller.go
+++ book-store-management/pkg/controller/book-controller.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/madhank93/learn-go/book-store-management/pkg/models"
+	"github.com/madhank93/learn-go/book-store-management/pkg/utils"
 )
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +18,17 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
-func CreateBook(w http.ResponseWriter, r *http.Request) {}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	book := &models.Book{}
+	utils.ParseBody(r, book)
+	b := book.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
 func GetBooksByID(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 0)
 	if err != nil {
@@ -29,5 +40,29 @@ func GetBooksByID(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
-func UpdateBook(w http.ResponseWriter, r *http.Request) {}
-func DeleteBook(w http.ResponseWriter, r *http.Request) {}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	book := &models.Book{}
+	utils.ParseBody(r, book)
+	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	updateBooks := book.UpdateBook(int(id))
+	res, _ := json.Marshal(updateBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	book := models.DeleteBook(int(id))
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
--- book-store-management/pkg/models/book.go
+++ book-store-management/pkg/models/book.go
@@ -5,7 +5,6 @@ import (
 	"log"
 
 	_ "github.com/lib/pq"
-
 	"github.com/madhank93/learn-go/book-store-management/pkg/config"
 )
 
@@ -32,7 +31,10 @@ func GetAllBooks() []Book {
 	}
 	defer rows.Close()
 	for rows.Next() {
-		rows.Scan(&book.Name, &book.Author, &book.Publication, &book.ISBN)
+		err := rows.Scan(&book.Name, &book.Author, &book.Publication, &book.ISBN)
+		if err != nil {
+			log.Fatalln(err)
+		}
 		books = append(books, book)
 	}
 	return books
@@ -40,13 +42,44 @@ func GetAllBooks() []Book {
 
 func GetBookByID(bookID int) Book {
 	var book Book
-	rows, err := db.Query("SELECT name, author, publication, isbn FROM public.books WHERE isbn = $1;", bookID)
+	rows, err := db.Query("SELECT name, author, publication, isbn FROM public.books WHERE id = $1;", bookID)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
-		rows.Scan(&book.Name, &book.Author, &book.Publication, &book.ISBN)
+		err := rows.Scan(&book.Name, &book.Author, &book.Publication, &book.ISBN)
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 	return book
+}
+
+func (b *Book) CreateBook() *Book {
+	rows, err := db.Query("INSERT INTO public.books(name, author, publication, isbn) VALUES ($1, $2, $3, $4);", b.Name, b.Author, b.Publication, b.ISBN)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rows.Close()
+	return b
+}
+
+func (b *Book) UpdateBook(bookID int) *Book {
+	rows, err := db.Query("UPDATE public.books SET name=$1, author=$2, publication=$3, isbn=$4 WHERE id = $5;", b.Name, b.Author, b.Publication, b.ISBN, bookID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rows.Close()
+	return b
+}
+
+func DeleteBook(bookID int) string {
+	rows, err := db.Query("DELETE FROM public.books WHERE id=$1;", bookID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rows.Close()
+
+	return "Book has been deleted"
 }
